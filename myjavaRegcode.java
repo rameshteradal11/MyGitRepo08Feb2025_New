7a4476afdaa9dcabac45b1773d96477a711a7259
rameshteradal11 <105331062+rameshteradal11@users.noreply.github.com>
2025-02-09 13:44:35 +0530

    Update myjavaRegcode.java
    Additional commit and corrected all lines

--- myjavaRegcode.java
+++ myjavaRegcode.java
@@ -1,6 +1,5 @@
-this is my javagproegrgipeg34gp34hg
-
-
-fgwefgwefgwpeifgwepigfuepw
-
-wfeuwuiefifeuwt
+1st Line code by DEV A
+2nd Line code by DEV B
+3rd Line code by DEV A
+4th Line code by DEV B
+5th Line code by DEV A
